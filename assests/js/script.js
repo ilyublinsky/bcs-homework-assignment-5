355d03ac47fedf2f5df1326d7c38b1326b9ce8d4
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-01-28 22:20:10 -0800

    test save to local storage

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,3 +4,13 @@ $(document).ready(function() {
     var todayDate = moment().format('dddd, MMMM Do YYYY');
     dateParagraph.text(todayDate);
     });
+
+//to save the user's input of text into each time slot....
+
+var input_textarea1 = document.querySelector('#note1');
+var save_button1 = document.querySelector('#saveNote1');
+input_textarea1.value = localStorage.getItem('content1');
+saveNote1.addEventListener('click', updateOutput1);
+function updateOutput1() {
+    localStorage.setItem('content1', input_textarea1.value);
+};
